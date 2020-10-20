ac7dbc4e7de59f93fb0d4521098a53685a2568a9
Vinicius Moura <v-vimou@microsoft.com>
2020-10-20 14:52:05 -0300

    New script and report to list all users and groups on Azure DevOps

---
+++ PowerShell/ListUserAndPermissions/ListUserAndPermissions.ps1
@@ -0,0 +1,30 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization
+)
+
+$UserGroups = @()
+
+echo $PAT | az devops login --org $Organization
+
+az devops configure --defaults organization=$Organization
+
+$allUsers = az devops user list --org $Organization | ConvertFrom-Json
+
+foreach($au in $allUsers.members)
+{
+    $activeUserGroups = az devops security group membership list --id $au.user.principalName --org $Organization --relationship memberof | ConvertFrom-Json
+    [array]$groups = ($activeUserGroups | Get-Member -MemberType NoteProperty).Name
+
+    foreach ($aug in $groups)
+    {
+        $UserGroups += New-Object -TypeName PSObject -Property @{
+                                            principalName=$au.user.principalName
+                                            displayName=$au.user.displayName
+                                            GroupName=$activeUserGroups.$aug.principalName
+                                            }
+    }
+}
+
+$UserGroups | ConvertTo-Json | Out-File -FilePath "$home\desktop\UserGroups.json"
