4ad3b4ee3dc6cd884f33108fd5ddff1c8cab0bd4
Vinicius Moura <v-vimou@microsoft.com>
2020-10-26 17:49:30 -0300

    Extract Branch Policies of all repositories on Azure DevOps

---
+++ PowerShell/RepoBranchPolicies/RepoBranchPolicies.ps1
@@ -0,0 +1,62 @@
+Param
+(
+    [string]$PAT,
+    [string]$Organization
+)
+$RepoBranchPolicies = @()
+
+$minimumRev = "Minimum number of reviewers"
+$reqReviewers = "Required reviewers"
+$workItemLink = "Work item linking"
+$commentReq = "Comment requirements"
+$Build = "Build"
+
+echo $PAT | az devops login --org $Organization
+
+az devops configure --defaults organization=$Organization
+$ProjectsResult = az devops project list --org $Organization | ConvertFrom-Json
+
+Foreach ($project in $ProjectsResult.value)
+{
+    $ReposResult = az repos list --org $Organization --project $project.id | ConvertFrom-Json
+    Foreach ($repo in $ReposResult)
+    {
+        if ($repo.defaultBranch)
+        {
+            $ReposPolicyResult = az repos policy list --branch $repo.defaultBranch --org $Organization --project $project.id --repository-id $repo.id | ConvertFrom-Json
+        
+            [bool] $repominimumRev = $false
+            [bool] $reporeqReviewers = $false
+            [bool] $repoworkItemLink = $false
+            [bool] $repocommentReq = $false
+            [bool] $repoBuild = $false
+
+            Foreach ($repoPolicy in $ReposPolicyResult)
+            {
+                switch($repoPolicy.type.displayName)
+                {
+                    $minimumRev {$repominimumRev = $true}
+                    $reqReviewers {$reporeqReviewers = $true}
+                    $workItemLink {$repoworkItemLink = $true}
+                    $commentReq {$repocommentReq = $true}
+                    $Build {$repoBuild = $true}
+                }
+            }
+            $RepoBranchPolicies += New-Object -TypeName PSObject -Property @{
+                    TeamProjectId=$project.id
+                    TeamProjectName=$project.name
+                    RepositoryId=$repo.id
+                    RepositoryName=$repo.name
+                    RepositoryURL=$repo.remoteUrl
+                    RepositoryDefaultBranch=$repo.defaultBranch
+                    RepositoryDefaultBranchMinimumNumberOfReviewers=$repominimumRev
+                    RepositoryDefaultBranchRequiredReviewers=$reporeqReviewers
+                    RepositoryDefaultBranchWorkItemLinking=$repoworkItemLink
+                    RepositoryDefaultBranchCommentRequirements=$repocommentReq
+                    RepositoryDefaultBranchBuild=$repoBuild
+            }
+        }
+    }
+}
+
+$RepoBranchPolicies | ConvertTo-Json | Out-File -FilePath "$home\desktop\RepoBranchPolicies.json"
