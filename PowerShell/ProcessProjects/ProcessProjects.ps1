b4b837df7bfa3ca1fae560bcc1e156ad95d7475d
Vinicius Moura <v-vimou@microsoft.com>
2020-11-06 10:49:53 -0300

    List Process and respective projects

---
+++ PowerShell/ProcessProjects/ProcessProjects.ps1
@@ -0,0 +1,28 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization
+)
+
+$ProcessProjects = @()
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+
+$UriOrganization = "https://dev.azure.com/$($Organization)/" 
+$uriProcess = $UriOrganization + "/_apis/work/processes?`$expand=projects"
+
+$processesResult = Invoke-RestMethod -Uri $uriProcess -Method get -Headers $AzureDevOpsAuthenicationHeader
+
+Foreach ($process in $processesResult.value)
+{
+    Foreach ($prj in $process.projects)
+    {
+        $ProcessProjects += New-Object -TypeName PSObject -Property @{
+                                        ProcessName=$process.name
+                                        ProjectName=$prj.name
+                                    }
+    }
+    
+}
+
+$ProcessProjects | ConvertTo-Json | Out-File -FilePath "$home\desktop\ProcessProjects.json"
