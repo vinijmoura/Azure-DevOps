0d6d8b34a870d252622a76b30df4b210fabccd28
Vinicius Moura <vinijmoura@hotmail.com>
2021-08-19 19:04:37 -0300

    Create report to list all installed extensions on the Azure DevOps organization

---
+++ PowerShell/ListInstalledExtensions/InstalledExtensions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[InstalledExtensions](
+	[ExtensionId] [varchar](100) NULL,
+	[ExtensionName] [varchar](100) NULL,
+	[ExtensionPublisherName] [varchar](100) NULL,
+	[ExtensionVersion] [varchar](40) NULL,
+	[ExtensionLastPublished] [datetime] NULL
+)
+GO
---
+++ PowerShell/ListInstalledExtensions/ListInstalledExtensions.ps1
@@ -0,0 +1,37 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE InstalledExtensions"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+$UriInstalledExtensions = "https://extmgmt.dev.azure.com/$($Organization)/_apis/extensionmanagement/installedextensions?api-version=6.0-preview.1"
+$InstalledExtensionsResult = Invoke-RestMethod -Uri $UriInstalledExtensions -Method get -Headers $AzureDevOpsAuthenicationHeader
+
+Foreach ($extension in $InstalledExtensionsResult.value)
+{
+    $extName = $($extension.extensionName).Replace("'","")
+    $extPublisherName = $($extension.publisherName).Replace("'","")
+
+    $SQLQuery = "INSERT INTO InstalledExtensions (
+                                ExtensionId,
+                                ExtensionName,
+                                ExtensionPublisherName,
+                                ExtensionVersion,
+                                ExtensionLastPublished
+                                )
+                                VALUES(
+                                '$($extension.extensionId)',
+                                '$($extName)',
+                                '$($extPublisherName)',
+                                '$($extension.version)',
+                                CONVERT(DATETIME,SUBSTRING('$($extension.lastPublished)',1,19),127)
+                                )"
+    Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+}
+
+            
