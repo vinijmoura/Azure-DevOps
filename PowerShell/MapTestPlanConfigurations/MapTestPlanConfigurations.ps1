f00b807f3c0619e7277b0c66bcac63a06b7b9707
Vinicius Moura <vinijmoura@hotmail.com>
2021-08-25 18:54:45 -0300

    Create report to map all Test Plans Configurations within Azure DevOps Projects

---
+++ PowerShell/MapTestPlanConfigurations/MapTestPlanConfigurations.ps1
@@ -0,0 +1,41 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE TestPlansConfigurations"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+
+$UriOrganization = "https://dev.azure.com/$($Organization)/" 
+$uriProject = $UriOrganization + "_apis/projects?`$top=500"
+$ProjectsResult = Invoke-RestMethod -Uri $uriProject -Method get -Headers $AzureDevOpsAuthenicationHeader
+Foreach ($project in $ProjectsResult.value)
+{
+    $uriTestPlansConfigurations = $UriOrganization + "$($project.id)/_apis/testplan/configurations?api-version=6.1-preview.1"
+    $TestPlansConfigurationsResult = Invoke-RestMethod -Uri $uriTestPlansConfigurations -Method get -Headers $AzureDevOpsAuthenicationHeader
+    Foreach ($tpconfig in $TestPlansConfigurationsResult.value)
+    {
+        Foreach ($value in $tpconfig.values)
+        {
+            $SQLQuery = "INSERT INTO TestPlansConfigurations (
+                        TeamProjectName,
+                        TeamPlanConfigurationId,
+                        TeamPlanConfigurationName,
+                        TeamPlanConfigurationVariableName,
+                        TeamPlanConfigurationVariableValue
+                        )
+                        VALUES(
+                        '$($project.name)',
+                        $($tpconfig.id),
+                        '$($tpconfig.name)',
+                        '$($value.name)',
+                        '$($value.value)'
+                        )"
+            Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+        }
+    }
+}
---
+++ PowerShell/MapTestPlanConfigurations/TestPlanConfigurations.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[TestPlansConfigurations](
+	[TeamProjectName] [varchar](100) NOT NULL,
+	[TeamPlanConfigurationId] [tinyint] NOT NULL,
+	[TeamPlanConfigurationName] [varchar](100) NOT NULL,
+	[TeamPlanConfigurationVariableName] [varchar](100) NOT NULL,
+	[TeamPlanConfigurationVariableValue] [varchar](100) NOT NULL
+) 
+GO
