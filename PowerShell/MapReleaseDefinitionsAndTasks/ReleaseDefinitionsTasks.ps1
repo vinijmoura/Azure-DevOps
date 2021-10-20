0d506e634bc6d45e3392631d2c14a7851e8dd320
Vinicius Moura <vinijmoura@hotmail.com>
2021-10-20 16:30:10 -0300

    Create Report to Map Release Tasks and respective usage on Team Project and Release Definition

---
+++ PowerShell/MapReleaseDefinitionsAndTasks/ReleaseDefinitionsTasks.ps1
@@ -0,0 +1,56 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE ReleaseDefinitionsTasks"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+$UriOrganization = "https://dev.azure.com/$($Organization)/"
+$uriReleases = "https://vsrm.dev.azure.com/$($Organization)/"
+
+$uriProject = $UriOrganization + "_apis/projects?`$top=500"
+$ProjectsResult = Invoke-RestMethod -Uri $uriProject -Method get -Headers $AzureDevOpsAuthenicationHeader
+Foreach ($project in $ProjectsResult.value)
+{   
+    $uriReleaseDefinitions = $uriReleases + "$($project.id)/_apis/release/definitions?api-version=6.1-preview.4"
+    $ReleaseDefintionsResult = Invoke-RestMethod -Uri $uriReleaseDefinitions -Method get -Headers $AzureDevOpsAuthenicationHeader
+    Foreach ($releaseDef in $ReleaseDefintionsResult.value)
+    {
+        $uriReleaseDef = $uriReleases + "$($project.id)/_apis/release/definitions/$($releaseDef.id)?api-version=6.1-preview.4"
+        $ReleaseDefResult = Invoke-RestMethod -Uri $uriReleaseDef -Method get -Headers $AzureDevOpsAuthenicationHeader
+        Foreach ($environment in $ReleaseDefResult.environments)
+        {
+            Foreach ($deployPhase in $environment.deployPhases)
+            {
+                Foreach ($task in $deployPhase.workflowTasks)
+                {
+                    $SQLQuery = "INSERT INTO ReleaseDefinitionsTasks (
+                                    TeamProjectName,
+                                    ReleaseDefinitionId,
+                                    ReleaseDefinitionName,
+                                    ReleaseDefinitionEnvironmentName,
+                                    ReleaseDefinitionPhaseName,
+                                    ReleaseDefintionTaskId,
+                                    ReleaseDefintionTaskName,
+                                    ReleaseDefintionTaskversion
+                                    )
+                                    VALUES(
+                                    '$($project.name)',
+                                    '$($releaseDef.id)',
+                                    '$($releaseDef.name)',
+                                    '$($environment.name)',
+                                    '$($deployPhase.name)',
+                                    '$($task.taskId)',
+                                    '$($task.name)',
+                                    '$($task.version)'
+                                    )"
+                    Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+                }
+            }
+        }
+    }
+}
---
+++ PowerShell/MapReleaseDefinitionsAndTasks/ReleaseDefinitionsTasks.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[ReleaseDefinitionsTasks](
+	[TeamProjectName] [varchar](100) NULL,
+	[ReleaseDefinitionId] [varchar](40) NULL,
+	[ReleaseDefinitionName] [varchar](150) NULL,
+	[ReleaseDefinitionEnvironmentName] [varchar](150) NULL,
+	[ReleaseDefinitionPhaseName] [varchar](150) NULL,
+	[ReleaseDefintionTaskId] [varchar](40) NULL,
+	[ReleaseDefintionTaskName] [varchar](150) NULL,
+	[ReleaseDefintionTaskversion] [varchar](30) NULL
+)
+GO
