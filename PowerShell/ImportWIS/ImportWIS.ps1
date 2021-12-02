8aca860af7c78fe3546cf52896c859289dfaabfd
Vinicius Moura <v.moura@accenture.com>
2021-12-02 00:13:56 -0300

    Insert Work Items usig txt file

---
+++ PowerShell/ImportWIS/ImportWIS.ps1
@@ -0,0 +1,15 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization
+)
+#connect Azure DevOps
+echo $PAT | az devops login --org $Organization
+az devops configure --defaults organization=$Organization
+#Read File
+$data = Import-Csv -Delimiter '|' -Path "$home\desktop\wis.txt" -Header 'WorkItemType','Title','TeamProject','Area','Iteration'
+
+#insert Work Item
+$data | foreach { az boards work-item create --title $_.Title --type $_.WorkItemType --project $_.TeamProject --area $_.Area --iteration $_.Iteration }
+
+
