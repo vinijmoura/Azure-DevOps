07f7bbd707ea758636cf83d74d22916179efca0e
Vinicius Moura <v.moura@accenture.com>
2022-01-12 15:15:17 -0300

    Create Report to map Team Projects and respective customized fields that using picklists

---
+++ PowerShell/MapProjectsPickLists/MapProjectsPickLists.ps1
@@ -0,0 +1,48 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE ProjectsPickLists"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+$UriOrganization = "https://dev.azure.com/$($Organization)/" 
+
+$uriProject = $UriOrganization + "_apis/projects?`$top=500"
+$ProjectsResult = Invoke-RestMethod -Uri $uriProject -Method get -Headers $AzureDevOpsAuthenicationHeader
+Foreach ($project in $ProjectsResult.value)
+{
+    $uriProjectPickList = $UriOrganization + "$($project.id)/_apis/wit/fields?api-version=6.0"
+    $ProjectsPickListResult = Invoke-RestMethod -Uri $uriProjectPickList -Method get -Headers $AzureDevOpsAuthenicationHeader
+    $ProjectsPickListResult = $ProjectsPickListResult.value | where-object {$_.isPicklist -eq $true}
+    Foreach ($fieldPickList in $ProjectsPickListResult)
+    {
+        $uriPickList = $UriOrganization + "_apis/work/processes/lists/$($fieldPickList.picklistId)?api-version=6.0-preview.1"
+        $PickListResult = Invoke-RestMethod -Uri $uriPickList -Method get -Headers $AzureDevOpsAuthenicationHeader
+        $items = $PickListResult.items
+        $items = "$items".Replace(" ","`r`n")
+        
+        $SQLQuery = "INSERT INTO ProjectsPickLists(
+                    TeamProjectId,
+                    TeamProjectName,
+                    FieldName,
+                    FieldReferenceName,
+                    FieldType,
+                    FieldPickListId,
+                    FieldPickListItems
+                    )
+                    VALUES(
+                    '$($project.id)',
+                    '$($project.name)',
+                    '$($fieldPickList.name)',
+                    '$($fieldPickList.referenceName)',
+                    '$($fieldPickList.type)',
+                    '$($fieldPickList.picklistId)',
+                    '$($items)'
+                    )"
+        Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+    }
+}
---
+++ PowerShell/MapProjectsPickLists/ProjectsPickLists.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[ProjectsPickLists](
+	[TeamProjectId] [VARCHAR](40) NOT NULL,
+	[TeamProjectName] [VARCHAR](200) NOT NULL,
+	FieldName [VARCHAR](100) NOT NULL,
+    FieldReferenceName [VARCHAR](100) NOT NULL,
+    FieldType [VARCHAR](30) NOT NULL,
+    FieldPickListId [VARCHAR](40) NOT NULL,
+    FieldPickListItems NVARCHAR(MAX) NOT NULL
+) 
+GO
