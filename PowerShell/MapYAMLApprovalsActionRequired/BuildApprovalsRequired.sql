743051650a770aa4ffc9a0fdfc3d88c8a86d52f4
Vinicius Moura <vinijmoura@hotmail.com>
2023-02-24 14:31:22 -0300

    Create Report to map Yaml multi-stages and respective aprrovals required

---
+++ PowerShell/MapYAMLApprovalsActionRequired/BuildApprovalsRequired.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[BuildApprovalsRequired](
+	[TeamProjectName] [varchar](100) NULL,
+	[BuildDefinitionId] [varchar](40) NULL,
+	[BuildDefinitionName] [varchar](150) NULL,
+	[BuildId] [varchar](20) NULL,
+	[BuildNumber] [varchar](100) NULL,
+	[BuildStageName] [varchar](100) NULL,
+	[BuildEnvironmentName] [varchar](100) NULL
+)
+GO
+
