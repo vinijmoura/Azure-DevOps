584a7af9d122aeb12f27a87bc92360116c329152
Vinicius Moura <vinijmoura@hotmail.com>
2023-05-19 15:55:08 -0300

    Include parallel execution on User extraction

--- AllReports/PowerShell/Users.ps1
+++ AllReports/PowerShell/Users.ps1
@@ -20,45 +20,46 @@ $tableUsersPersonalAccessTokens = $db.Tables["UsersPersonalAccessTokens"]
 $UriUsers = "https://vsaex.dev.azure.com/$($Organization)/_apis/userentitlements?top=10000&api-version=4.1-preview.1"
 $allUsers = Invoke-RestMethod -Uri $UriUsers -Method get -Headers $AzureDevOpsAuthenicationHeader
 
-foreach ($au in $allUsers.value) {
-    $yearAccess = ([datetime]$au.lastAccessedDate).Year
+$allUsers.value | ForEach-Object -ThrottleLimit 10 -Parallel  {
+#foreach ($au in $allUsers.value) {
+    $yearAccess = ([datetime]$_.lastAccessedDate).Year
     if ($yearAccess -eq 1) {
-        $LastDate = $au.dateCreated
+        $LastDate = $_.dateCreated
     }
     else {
-        $LastDate = $au.lastAccessedDate
+        $LastDate = $_.lastAccessedDate
     }
     $usersObject = [PSCustomObject] [ordered]@{
-        UserId                 = $au.id
-        UserPrincipalName      = $au.user.principalName
-        UserDisplayName        = $au.user.displayName
-        UserPictureLink        = $au.user.url
-        UserDateCreated        = $au.dateCreated
+        UserId                 = $_.id
+        UserPrincipalName      = $_.user.principalName
+        UserDisplayName        = $_.user.displayName
+        UserPictureLink        = $_.user.url
+        UserDateCreated        = $_.dateCreated
         UserLastAccessedDate   = $LastDate
-        UserLicenseDisplayName = $au.accessLevel.licenseDisplayName
+        UserLicenseDisplayName = $_.accessLevel.licenseDisplayName
     }
-    $Users.Add($usersObject)
-    & .\LogFile.ps1 -LogFile $LogFile -Message "Inserting user: $($au.user.principalName) on table Users"
+    ($using:Users).Add($usersObject)
+    & .\LogFile.ps1 -LogFile $using:LogFile -Message "Inserting user: $($_.user.principalName) on table Users"
     
-    $UriActiveUserGroups = "https://vssps.dev.azure.com/$($Organization)/_apis/graph/Memberships/$($au.user.descriptor)?api-version=7.0-preview.1"
-    $activeUserGroups = Invoke-RestMethod -Uri $UriActiveUserGroups -Method get -Headers $AzureDevOpsAuthenicationHeader
+    $UriActiveUserGroups = "https://vssps.dev.azure.com/$($using:Organization)/_apis/graph/Memberships/$($_.user.descriptor)?api-version=7.0-preview.1"
+    $activeUserGroups = Invoke-RestMethod -Uri $UriActiveUserGroups -Method get -Headers $using:AzureDevOpsAuthenicationHeader
     foreach ($aug in $activeUserGroups.value) {
-        $groupPrincipalName = Invoke-RestMethod -Uri $aug._links.container.href -Method get -Headers $AzureDevOpsAuthenicationHeader
+        $groupPrincipalName = Invoke-RestMethod -Uri $aug._links.container.href -Method get -Headers $using:AzureDevOpsAuthenicationHeader
         if ($groupPrincipalName) {  
             $usersGroupsObject = [PSCustomObject] [ordered]@{
-                UserId    = $au.id
+                UserId    = $_.id
                 GroupName = $groupPrincipalName.principalName
             }
-            $UsersGroups.Add($usersGroupsObject)
+            ($using:UsersGroups).Add($usersGroupsObject)
         }
     }
     
-    if ($UsersGroups.Count -gt 0) {
-        & .\LogFile.ps1 -LogFile $LogFile -Message "Inserting Permission Groups to which user $($au.user.principalName) belongs on table UsersGroups"
+    if ($using:UsersGroups.Count -gt 0) {
+        & .\LogFile.ps1 -LogFile $using:LogFile -Message "Inserting Permission Groups to which user $($_.user.principalName) belongs on table UsersGroups"
     }
 
-    $UriUserPAT = "https://vssps.dev.azure.com/$($Organization)/_apis/tokenadmin/personalaccesstokens/$($au.user.descriptor)?api-version=6.1-preview.1"
-    $UserPATResult = Invoke-RestMethod -Uri $UriUserPAT -Method get -Headers $AzureDevOpsAuthenicationHeader
+    $UriUserPAT = "https://vssps.dev.azure.com/$($using:Organization)/_apis/tokenadmin/personalaccesstokens/$($_.user.descriptor)?api-version=6.1-preview.1"
+    $UserPATResult = Invoke-RestMethod -Uri $UriUserPAT -Method get -Headers $using:AzureDevOpsAuthenicationHeader
     Foreach ($up in $UserPATResult.value) {
         if ($up.scope -eq 'app_token') {
             $accessToken = 'Full access'
@@ -67,16 +68,16 @@ foreach ($au in $allUsers.value) {
             $accessToken = $up.scope.Replace(" ", "`r`n")
         }
         $usersPersonalAccessTokensObject = [PSCustomObject] [ordered]@{
-            UserId         = $au.id
+            UserId         = $_.id
             PATDisplayName = $up.displayName
             PATValidFrom   = $up.validFrom
             PATValidTo     = $up.validTo
             PATScope       = $accessToken
         }
-        $UsersPersonalAccessTokens.Add($usersPersonalAccessTokensObject)
+        ($using:UsersPersonalAccessTokens).Add($usersPersonalAccessTokensObject)
     }
-    if ($UsersPersonalAccessTokens.Count -gt 0) {
-        & .\LogFile.ps1 -LogFile $LogFile -Message "Inserting Personal Access Tokens belonging to user $($au.user.principalName) on table UsersPersonalAccessTokens"
+    if ($using:UsersPersonalAccessTokens.Count -gt 0) {
+        & .\LogFile.ps1 -LogFile $using:LogFile -Message "Inserting Personal Access Tokens belonging to user $($_.user.principalName) on table UsersPersonalAccessTokens"
     }
 }
 Write-SqlTableData -InputData $Users -InputObject $tableUsers
