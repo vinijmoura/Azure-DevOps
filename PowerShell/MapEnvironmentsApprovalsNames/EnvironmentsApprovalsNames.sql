defe74355dd2982941e3b7cb50d1d29837266676
Vinicius Moura <vinijmoura@hotmail.com>
2023-07-24 19:20:41 -0300

    Create Report to list all environments and respective approvers

---
+++ PowerShell/MapEnvironmentsApprovalsNames/EnvironmentsApprovalsNames.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[EnvironmentsApprovalsNames](
+	[TeamProjectName] [varchar](100) NULL,
+	[EnvironmentId] [int] NULL,
+	[EnvironmentName] [varchar](150) NULL,
+	[ApproverUniqueName] [varchar](100) NULL,
+	[ApproverDisplayName] [varchar](100) NULL
+) ON [PRIMARY]
+GO
---
+++ PowerShell/MapEnvironmentsApprovalsNames/MapEnvironmentsApprovalsNames.ps1
@@ -0,0 +1,62 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE EnvironmentsApprovalsNames"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+$UriOrganization = "https://dev.azure.com/$($Organization)/"
+
+$uriProject = $UriOrganization + "_apis/projects?`$top=500"
+$ProjectsResult = Invoke-RestMethod -Uri $uriProject -Method get -Headers $AzureDevOpsAuthenicationHeader
+Foreach ($project in $ProjectsResult.value)
+{
+    $uriEnvironments = $UriOrganization + "$($project.id)/_apis/distributedtask/environments?api-version=6.1-preview.1"
+    $EnvironmentsResult = Invoke-RestMethod -Uri $uriEnvironments -Method get -Headers $AzureDevOpsAuthenicationHeader
+    Foreach ($environment in $EnvironmentsResult.value)
+    {
+        $body = @(
+                  @{
+                    type="queue"
+                    id="1"
+                    name="Default"
+                  },
+                  @{
+                    type="environment"
+                    id="$($environment.id)"
+                    name="$($environment.name)"
+                  }
+                ) | ConvertTo-Json
+        $uriEnvironmentChecks = $UriOrganization + "$($project.id)/_apis/pipelines/checks/queryconfigurations?`$expand=settings&api-version=6.1-preview.1"
+        $EnvironmentChecksResult = Invoke-RestMethod -Uri $uriEnvironmentChecks -Method Post -Body $body -Headers $AzureDevOpsAuthenicationHeader -ContentType application/json
+        Foreach ($envcheck in $EnvironmentChecksResult.value)
+        {
+            if ($envcheck.type.name -eq 'Approval')
+            {
+                $ApproversResult = Invoke-RestMethod -Uri $envcheck.url -Method get -Headers $AzureDevOpsAuthenicationHeader
+                Foreach ($approver in $ApproversResult.settings.approvers)
+                {
+                    $SQLQuery = "INSERT INTO EnvironmentsApprovalsNames (
+                            TeamProjectName,
+                            EnvironmentId,
+                            EnvironmentName,
+                            ApproverUniqueName,
+                            ApproverDisplayName
+                            )
+                            VALUES(
+                            '$($project.name)',
+                            $($environment.id),
+                            '$($environment.name)',
+                            '$($approver.uniqueName)',
+                            '$($approver.displayName)'
+                            )"
+                    Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr         
+                }                
+            }
+        }
+    }
+}
